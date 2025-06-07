cd9129a244c9982d8401e4b2937fa821f96ef697
Picaso <abcdbohemia@gmail.com>
2025-06-07 17:42:08 +0400

    Feat: Add useQuery for fetchTags, add container for input, tags(checkbox and label), and button.
    Add handleSubmit function to the form element.
    
    Style the form, button, postbox, and tags

--- tanstack-query-tutorial/src/components/PostList.jsx
+++ tanstack-query-tutorial/src/components/PostList.jsx
@@ -1,12 +1,62 @@
 import React from "react";
 import {useQuery} from "@tanstack/react-query";
-import {fetchPosts} from "../api/api";
+import {fetchPosts, addPosts, fetchTags} from "../api/api";
 
 const PostList = () => {
-    const { data:postData, isError, isLoading, error } = useQuery({
+    const { data:postData, isLoading, error } = useQuery({
         queryKey: ["posts"],
         queryFn: fetchPosts,
     });
+
+    const {data: tagsData } = useQuery({
+        queryKey: ["tags"],
+        queryFn: fetchTags,
+    })
+/*
+    const {
+        mutate, 
+        isError: isPostError, 
+        isPending, 
+        error:postError, 
+        reset,
+    } = useMutation({
+        mutationFn: addPosts,
+    });
+*/
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.target)
+        const title = formData.get("title");
+        const tags = Array.from(formData.keys()).filter(
+            (key) => formData.get(key)==="on"
+        );
+
+       
+    }
+
+    return (
+        <div className="container">
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Enter your post..."
+                    className="postbox"
+                    name="title"
+                />
+                <div className="tags">
+                    {tagsData?.map((tag) => {
+                        return (
+                            <div key={tag}>
+                                <input name={tag} id={tag} type="checkbox" />
+                                <label htmlFor={tag}>{tag}</label>
+                            </div>
+                        )
+                    })}
+                </div>
+                <button>Post</button>
+            </form>
+        </div>
+    )
 
     if (isLoading) {
         return 
