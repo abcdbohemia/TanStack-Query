61f0da7c2cca759e6231d245c58177e863051834
Picaso <abcdbohemia@gmail.com>
2025-06-07 10:37:37 +0400

    Feat: Refactor PostList component to handle, loading, error, and data rendering states.
    Update API endpoint from 3000 to 3001 to avoid  endpoint conflict with main React App

--- tanstack-query-tutorial/src/api/api.js
+++ tanstack-query-tutorial/src/api/api.js
@@ -1,18 +1,18 @@
 const fetchPosts = async () => {
-    const response = await fetch(`http://localhost:3000/posts?_sort=id`);
+    const response = await fetch(`http://localhost:3001/posts?_sort=id`);
 
     const postData = await response.json();
     return postData;
 };
 
 const fetchTags = async () => {
-    const response = await fetch(`http://localhost:3000/tags`);
+    const response = await fetch(`http://localhost:3001/tags`);
     const tagsData = await response.json();
     return tagsData;
 };
 
 const addPosts = async (post) => {
-    const response = await fetch(`http://localhost:3000/posts`, {
+    const response = await fetch(`http://localhost:3001/posts`, {
         method: "POST",
         headers: {
             "Content-Type": "Application/json",
--- tanstack-query-tutorial/src/components/PostList.jsx
+++ tanstack-query-tutorial/src/components/PostList.jsx
@@ -8,23 +8,33 @@ const PostList = () => {
         queryFn: fetchPosts,
     });
 
+    if (isLoading) {
+        return 
+            <div className="container">
+                <p>Loading...</p>
+            </div>;
+    }
+    if (!postData || !Array.isArray(postData)) {
+        return
+            <div className="container">
+                <p>No posts found or data format incorrect.</p>
+            </div>;
+    }
     return (
-    <div className="container">
-        {isLoading && <p>Loading...</p>}
-        {isError && <p>{error.message}</p>}
-    
-        {postData.map((post) => {
-            return ( 
-                <div key={post.id}>
-                    <div>{post.title}</div>
-                    {post.tag.map((tag) => (
-                        <span key={tag}>{tag}</span> 
-                    ))}
+        <div className="container">
+            {postData.map((post) => {
+                return (
+                    <div key={post.id}>
+                        <div>{post.title}</div>
+                    <div/>
+    {post.tag && Array.isArray(post.tag) && post.tag.map((tag) => (
+        <span key={tag}>{tag}</span>
+    ))}
                 </div>
             );
         })}
     </div>
-    )
+    );
 };
 
 export default PostList; 
