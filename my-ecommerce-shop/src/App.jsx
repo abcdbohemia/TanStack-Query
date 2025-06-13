db33cf8c3b521620c441ceb0b059f4a87797f007
Picaso <abcdbohemia@gmail.com>
2025-06-13 16:46:57 +0400

    fear: add comments to App.jsx

--- my-ecommerce-shop/src/App.jsx
+++ my-ecommerce-shop/src/App.jsx
@@ -5,17 +5,21 @@ import ProductList from './components/ProductList';
 import ProductDetail from './components/ProductDetail';
 import './App.css'
 
+//we're importing the Link with Navbar import
+// Link is the trigger, while Route is the "destination"
+// :id the colon makes id the name of that URL parameter
+// Routes and Route are control components, they are not visible
+//Route takes JSX element
 
 function App() {
-
   return (
     <>
-      <Navbar />
-      <main className="app-main-content">
+      <Navbar /> 
+      <main className="app-main-content"> 
         <Routes>
           <Route path="/" element={<ProductList />} />
           <Route path="/products" element={<ProductList />} />
-          <Route path="/products/:id" element={<ProductDetail />} />
+          <Route path="/products/:id" element={<ProductDetail />} /> 
         </Routes>
       </main>
     </>
